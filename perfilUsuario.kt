e1c1b00a811fb735737081afc826844a18891269
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-23 20:40:01 -0300

    Segunda vez

---
+++ perfilUsuario.kt
@@ -0,0 +1,11 @@
+fun main(){
+    var nome: String = "fe"
+    var idade: Int = 20
+    var email: String = "fe@"
+    var usuario: Boolean = true 
+
+    println("Nome: $nome")
+    println("Idade: $idade")
+    println("Email: $email")
+    println("Usuario: $usuario")
+}
